b84cd778353257bfeb95853f51657c0fa00958c1
smriti <smriti2815@gmail.com>
2026-03-13 19:16:24 +0530

    feat: initial Terraform IaC for shopizer stack on Colima
    - Docker provider targeting Colima socket
    - shopizer-network bridge network
    - Pulls and runs all 3 images: backend (8090), admin (8091), shop (3001)
    - restart=unless-stopped for all containers
    - Outputs: backend_url, admin_url, shop_url

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "backend_url" {
+  value = "http://localhost:8090"
+}
+
+output "admin_url" {
+  value = "http://localhost:8091"
+}
+
+output "shop_url" {
+  value = "http://localhost:3001"
+}
